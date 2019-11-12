9cdfb3128c5eeb3cc38fe706af0bdc8dbb1ca1e2
wawa <tanwa5120@gmail.com>
2019-11-12 17:26:12 +0700

    Programnaja

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> wawa = new List<int>();
+            IHomework13 re = new homework2();
+            var number = new List<int>(){000000, 000099, 009900, 990000,
+            000999, 009990, 099900, 999000,
+            990999, 919909, 999799, 999999,};
+            wawa = re.Homework13(number);
+            foreach (var item in wawa)
+            {
+                string r = String.Format("{0:D6}", item);
+                Console.WriteLine(r);
+            }
+
+        }
+    }
+}
